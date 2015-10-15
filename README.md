d71104b805da402a64e3755f3ecb7f98258c1f20
nanastop <nanastop@intracom-telecom.com>
2015-10-15 18:39:22 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,128 @@
 # oftraf
+
 RESTful OpenFlow traffic monitor
+
+Filters OpenFlow packets from a network interface and reports statistics. 
+
+Features:
+
+- **Summary and detailed OF statistics in real-time**: summary statistics refer to 
+  total incoming and outgoing OF traffic (from the viewpoint of the SDN 
+  controller). Detailed statistics offer a finer-grain breakdown based on the OF 
+  message type. Both classes are being updated and displayed in real-time.
+- Support for **OF1.0 and OF1.3 protocols**
+- **REST interface** for accessing statistics. This enables remote online 
+  monitoring of a controller's incoming and outgoing OF traffic.
+
+# Requirements
+
+- Python 2
+- `pypcap`, `dpkt` and `bottle` Python packages
+
+On an Ubuntu-based machine, install the required packages as follows: 
+
+```bash
+sudo apt-get install python-pypcap python-dpkt python-bottle
+```
+
+# Usage
+
+```bash
+sudo python oftraf.py --rest-host <host> --rest-port <port> --of-port <ofport> --ifname <interface>
+```
+
+Command line arguments: 
+
+- `--rest-host`: the IP or hostname of the interface the REST server should listen to
+- `--rest-port`: the port the REST server should listen to
+- `--of-port`: the OpenFlow port number based on which packet filtering will take place
+- `--ifname`: the network interface to sniff packets from
+
+Example: 
+
+1. Launch an SDN controller and a Mininet topology on the same machine
+2. Launch `oftraf`:  
+  ```bash
+  sudo python oftraf.py --rest-host localhost --rest-port 5555 --of-port 6653 --ifname lo
+  ```
+  This starts sniffing and counting OF packets on the `lo` interface. The statistics are 
+  being displayed in real-time in a curses-based console which refreshes every 1 second.
+  Sample output: 
+  
+  ```
+  Elapsed seconds:994.4902
+  OF in pps:             29390.0
+  OF in Bps:           3885368.0
+  OF out pps:            31300.0
+  OF out Bps:          3964004.0
+
+  Packet Type                           Count          Bytes
+  --------------------------------------------------------------
+  Total OF in:                          700581         212923343
+  Total OF out:                         366417         114676077
+  OF13_OFPT_ECHO_REPLY:                 3088           185528
+  OF13_OFPT_ECHO_REQUEST:               3092           185520
+  OF13_OFPT_FLOW_MOD:                   353            81460
+  OF13_OFPT_MULTIPART_REPLY:            445024         171816903
+  OF13_OFPT_MULTIPART_REQUEST:          40668          87600195
+  OF13_OFPT_PACKET_IN:                  245614         40563432
+  OF13_OFPT_PACKET_OUT:                 9469           10533154
+  ```
+  
+  **OF in** and **OF out** refer to OF traffic traveling into and out of the SDN controller,
+  respectively. **pps** and **Bps** are packets-per-second and bytes-per-second.
+  
+3. The REST server starts together with `oftraf`. Let's try to send some statistics requests. 
+
+  On another console, issue the following REST request to access summary statistics: 
+  ```bash
+  curl  http://localhost:5555/get_of_counts
+  ```
+  Response: 
+
+  ```json
+  {
+    "OF_out_counts": [366417, 114676077], 
+    "OF_in_counts": [700581, 212923343]
+  }
+  ```
+  In each statistic returned, the first number (e.g. 366417) is the packet count, and the 
+  second (114676077) is the byte count. 
+  
+  `OF_out_counts` refers to the packets travelling from the controller to the 
+  switches, `OF_in_counts` refers to the packets travelling at the opposite 
+  direction. 
+  
+  To access detailed OF13 statistics, issue the following REST request:
+
+  ```bash
+  curl http://localhost:5555/get_of13_counts
+  ```
+
+  Response: 
+
+  ```json
+  {
+    "OF13_OFPT_ECHO_REPLY": [3088, 185528],
+    "OF13_OFPT_ECHO_REQUEST": [3092, 185520],
+    "OF13_OFPT_FLOW_MOD": [353, 81460],
+    "OF13_OFPT_MULTIPART_REPLY": [445024, 171816903],
+    "OF13_OFPT_MULTIPART_REQUEST": [40668, 87600195],
+    "OF13_OFPT_PACKET_IN": [245614, 40563432],
+    "OF13_OFPT_PACKET_OUT": [9469, 10533154]
+  }
+  ```
+  
+  Similarly, use the following REST request for OF10 statistics: 
+  
+  ```bash
+  curl http://localhost:5555/get_of10_counts
+  ```
+
+4. To stop `oftraf`, hit Ctrl-C in the console where it runs, or issue a 
+   REST request as follows: 
+
+   ```bash
+   $ curl  http://localhost:5555/stop
+   ```
+
---
+++ oftraf.py
@@ -0,0 +1,281 @@
+# Copyright (c) 2015 Intracom S.A. Telecom Solutions. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+
+""" OpenFlow traffic monitor """
+
+import argparse
+import bottle
+import curses
+import dpkt
+import json
+import os
+import pcap
+import time
+import threading
+
+
+of10_types = {
+    '\x00': 'OFPT_HELLO',
+    '\x01': 'OFPT_ERROR',
+    '\x02': 'OFPT_ECHO_REQUEST',
+    '\x03': 'OFPT_ECHO_REPLY',
+    '\x04': 'OFPT_VENDOR',
+    '\x05': 'OFPT_FEATURES_REQUEST',
+    '\x06': 'OFPT_FEATURES_REPLY',
+    '\x07': 'OFPT_GET_CONFIG_REQUEST',
+    '\x08': 'OFPT_GET_CONFIG_REPLY',
+    '\x09': 'OFPT_SET_CONFIG',
+    '\x0a': 'OFPT_PACKET_IN',
+    '\x0b': 'OFPT_FLOW_REMOVED',
+    '\x0c': 'OFPT_PORT_STATUS',
+    '\x0d': 'OFPT_PACKET_OUT',
+    '\x0e': 'OFPT_FLOW_MOD',
+    '\x0f': 'OFPT_PORT_MOD',
+    '\x10': 'OFPT_STATS_REQUEST',
+    '\x11': 'OFPT_STATS_REPLY',
+    '\x12': 'OFPT_BARRIER_REQUEST',
+    '\x13': 'OFPT_BARRIER_REPLY',
+    '\x14': 'OFPT_QUEUE_GET_CONFIG_REQUEST',
+    '\x15': 'OFPT_QUEUE_GET_CONFIG_REPLY'
+}
+"""dict of str:str: maps a packet type to an OpenFlow 1.0 message type
+"""
+
+of13_types = {
+    '\x00': 'OFPT_HELLO',
+    '\x01': 'OFPT_ERROR',
+    '\x02': 'OFPT_ECHO_REQUEST',
+    '\x03': 'OFPT_ECHO_REPLY',
+    '\x04': 'OFPT_EXPERIMENTER',
+    '\x05': 'OFPT_FEATURES_REQUEST',
+    '\x06': 'OFPT_FEATURES_REPLY',
+    '\x07': 'OFPT_GET_CONFIG_REQUEST',
+    '\x08': 'OFPT_GET_CONFIG_REPLY',
+    '\x09': 'OFPT_SET_CONFIG',
+    '\x0a': 'OFPT_PACKET_IN',
+    '\x0b': 'OFPT_FLOW_REMOVED',
+    '\x0c': 'OFPT_PORT_STATUS',
+    '\x0d': 'OFPT_PACKET_OUT',
+    '\x0e': 'OFPT_FLOW_MOD',
+    '\x0f': 'OFPT_GROUP_MOD',
+    '\x10': 'OFPT_PORT_MOD',
+    '\x11': 'OFPT_TABLE_MOD',
+    '\x12': 'OFPT_MULTIPART_REQUEST',
+    '\x13': 'OFPT_MULTIPART_REPLY',
+    '\x14': 'OFPT_BARRIER_REQUEST',
+    '\x15': 'OFPT_BARRIER_REPLY',
+    '\x16': 'OFPT_QUEUE_GET_CONFIG_REQUEST',
+    '\x17': 'OFPT_QUEUE_GET_CONFIG_REPLY',
+    '\x18': 'OFPT_ROLE_REQUEST',
+    '\x19': 'OFPT_ROLE_REPLY',
+    '\x1a': 'OFPT_GET_ASYNC_REQUEST',
+    '\x1b': 'OFPT_GET_ASYNC_REPLY',
+    '\x1c': 'OFPT_SET_ASYNC',
+    '\x1d': 'OFPT_METER_MOD'
+}
+"""dict of str:str: maps a packet type to an OpenFlow 1.3 message type
+"""
+
+of10_counts = {}
+"""dict of str:list: maps an OF10 message type to a list with total counts and
+bytes for that type
+"""
+
+of13_counts = {}
+"""dict of str:list: maps an OF13 message type to a list with total counts and
+bytes for that type
+"""
+
+ofin_counts = [0L, 0L]
+"""list of long: list with total counts and bytes for the incoming OF packets
+"""
+
+ofout_counts = [0L, 0L]
+"""list of long: list with total counts and bytes for the outgoing OF packets
+"""
+
+
+def of_sniff(ifname, ofport):
+    """Sniff the specified interface for OF packets at the specified OF port.
+
+    Packets will be filtered based on whether their TCP src port or TCP dst port
+    equals the OF port given.
+
+    Args:
+        ifname (str): network interface name
+        ofport (int): OF port number used for packet filtering
+    """
+
+    try:
+        for _, pkt in pcap.pcap(name=ifname, immediate=True):
+
+            eth = dpkt.ethernet.Ethernet(pkt)
+            tcp = eth.data.data
+            nbytes = len(eth.data)
+
+            # element 0: packet count
+            # element 1: total packet bytes
+            if tcp.dport == int(ofport):
+                ofin_counts[0] += 1
+                ofin_counts[1] += nbytes
+            elif tcp.sport == int(ofport):
+                ofout_counts[0] += 1
+                ofout_counts[1] += nbytes
+            else:
+                continue
+
+            payload = tcp.data
+
+            # do not further analyze the packet if it does not have any
+            # OpenFlow payload
+            if len(payload) <= 1:
+                continue
+
+            of_version = payload[0]
+            of_type = payload[1]
+
+            # OF1.0
+            if of_version == '\x01':
+                if not of10_types[of_type] in of10_counts:
+                    of10_counts[of10_types[of_type]] = [1L, long(nbytes)]
+                else:
+                    of10_counts[of10_types[of_type]][0] += 1L
+                    of10_counts[of10_types[of_type]][1] += long(nbytes)
+            # OF1.3
+            elif of_version == '\x04':
+                if not of13_types[of_type] in of13_counts:
+                    of13_counts[of13_types[of_type]] = [1L, long(nbytes)]
+                else:
+                    of13_counts[of13_types[of_type]][0] += 1L
+                    of13_counts[of13_types[of_type]][1] += long(nbytes)
+
+    except KeyboardInterrupt:
+        os._exit(1)
+
+def print_stats():
+    """ Print statistics in an endless loop
+    """
+    start = time.time()
+    prev_in = [0,0]
+    prev_out = [0,0]
+    sleep_secs = 1
+
+    try:
+        win = curses.initscr()
+        while True:
+            time.sleep(sleep_secs)
+            curr_in = ofin_counts
+            curr_out = ofout_counts
+            curr_of10 = of10_counts
+            curr_of13 = of13_counts
+
+            in_rate = [(b-a)/float(sleep_secs) for (a,b) in zip(prev_in, curr_in)]
+            out_rate = [(b-a)/float(sleep_secs) for (a,b) in zip(prev_out, curr_out)]
+
+            elapsed_secs = '%.4f' % (time.time() - start)
+            out = '{0}{1:15}\n'.format('Elapsed seconds:', elapsed_secs)
+            out += '{0:15}{1:15}\n'.format('OF in pps:', in_rate[0])
+            out += '{0:15}{1:15}\n'.format('OF in Bps:', in_rate[1])
+            out += '{0:15}{1:15}\n'.format('OF out pps:', out_rate[0])
+            out += '{0:15}{1:15}\n'.format('OF out Bps:', out_rate[1])
+            out += '\n'
+            out += '{0:38}{1:15}{2:15}\n'.format('Packet Type', 'Count', 'Bytes')
+            out += '--------------------------------------------------------------\n'
+            out += '{0:38}{1:15}{2:15}\n'.format('Total OF in:',
+                                                 str(curr_in[0]),
+                                                 str(curr_in[1]))
+            out += '{0:38}{1:15}{2:15}\n'.format('Total OF out:',
+                                                 str(curr_out[0]),
+                                                 str(curr_out[1]))
+            for key in sorted(of13_counts):
+                out += '{0:38}{1:15}{2:15}\n'.format('OF13_' + key + ':',
+                                                     str(curr_of13[key][0]),
+                                                     str(curr_of13[key][1]))
+            for key in sorted(of10_counts):
+                out += '{0:38}{1:15}{2:15}\n'.format('OF10_' + key + ':',
+                                                     str(curr_of10[key][0]),
+                                                     str(curr_of10[key][1]))
+            prev_in = list(curr_in)
+            prev_out = list(curr_out)
+            win.addstr(0,0, out)
+            win.refresh()
+    except KeyboardInterrupt:
+        os._exit(1)
+
+@bottle.route('/get_of_counts', method='GET')
+def get_of_counts():
+    """ Callback function for summary statistics
+
+    Returns:
+        str: JSON object with total OF in and OF out counts
+    """
+    cnts = {}
+    cnts["OF_in_counts"] = ofin_counts
+    cnts["OF_out_counts"] = ofout_counts
+    return json.dumps(cnts)
+
+@bottle.route('/get_of10_counts', method='GET')
+def get_of10_counts():
+    """ Callback function for detailed OF10 statistics
+
+    Returns:
+        str: JSON object with detailed OF10 counts
+    """
+    return json.dumps(of10_counts)
+
+@bottle.route('/get_of13_counts', method='GET')
+def get_of13_counts():
+    """ Callback function for detailed OF13 statistics
+
+    Returns:
+        str: JSON object with detailed OF13 counts
+    """
+    return json.dumps(of13_counts)
+
+@bottle.route('/stop', method='GET')
+def stop():
+    """ Callback function for stopping oftraf
+    """
+    os._exit(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rest-host',
+                        required=True,
+                        type=str,
+                        dest='rest_host',
+                        action='store',
+                        help='IP address or hostname of the interface the REST'
+                             ' server should listen to')
+    parser.add_argument('--rest-port',
+                        required=True,
+                        type=str,
+                        dest='rest_port',
+                        action='store',
+                        help='Port number the REST server should listen to')
+    parser.add_argument('--of-port',
+                        required=True,
+                        type=str,
+                        dest='of_port',
+                        action='store',
+                        help='OpenFlow port number based on which packet'
+                             ' filtering will take place')
+    parser.add_argument('--ifname',
+                        required=True,
+                        type=str,
+                        dest='ifname',
+                        action='store',
+                        help='Network interface to sniff OF packets from')
+    args = parser.parse_args()
+
+    sniffer = threading.Thread(target=of_sniff, args=(args.ifname, args.of_port))
+    poller = threading.Thread(target=print_stats)
+    sniffer.daemon = True
+    poller.daemon = True
+
+    sniffer.start()
+    poller.start()
+    bottle.run(host=args.rest_host, port=args.rest_port, quiet=True)
