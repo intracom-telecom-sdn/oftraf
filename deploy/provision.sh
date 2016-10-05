262e8a6cc7dfd26c391b78ff6e13d80a5ba23518
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-10-05 16:13:32 +0300

    Link correction for requirements.txt

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -45,7 +45,7 @@ pip3 $pip_options install --upgrade pip
 mkdir $BASE_DIR/$VENV_DIR_OFTRAF
 virtualenv --system-site-packages $BASE_DIR/$VENV_DIR_OFTRAF
 
-wget https://raw.githubusercontent.com/intracom-telecom-sdn/oftraf/master/deploy/requirements.txt -P $BASE_DIR
+wget https://raw.githubusercontent.com/intracom-telecom-sdn/oftraf/virtenv-rqrmnts/deploy/requirements.txt -P $BASE_DIR
 source $BASE_DIR/$VENV_DIR_OFTRAF/bin/activate
 pip3 $pip_options install -r $BASE_DIR/requirements.txt
 rm -rf $BASE_DIR/requirements.txt
