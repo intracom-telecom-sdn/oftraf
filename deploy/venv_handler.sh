efb56cddd0e546173879b3ded5602ffeeb9c7896
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-10-06 11:50:49 +0300

    Add testing of venv_handler

--- deploy/venv_handler.sh
+++ deploy/venv_handler.sh
@@ -13,11 +13,14 @@
 # $5 --rest-port: the port the REST server should listen to
 # $6 --of-port: the OpenFlow port number based on which packet filtering will take place
 # $7 --ifname: the network interface to sniff packets from
-# $8 --server: run oftraf as server only without printing stats
+#    --server: run oftraf as server only without printing stats
 
-if [ "$#" -eq 4 ]
+if [ "$#" -eq 8 ]
 then
-    source $1/bin/activate; PYTHONPATH=$2 python $3 --rest-host $4 --rest-port $5 --of-port $6 --of-port $7 --server
+    source $1/bin/activate; PYTHONPATH=$2 python $3 --rest-host $4 --rest-port $5 --of-port $6 --of-port $7 --server &
+elif [ "$#" -eq 7 ]
+then
+    PYTHONPATH=$1 python $2 --rest-host $3 --rest-port $4 --of-port $5 --of-port $6 --server
 else
     echo "Invalid number of arguments."
     exit 1
